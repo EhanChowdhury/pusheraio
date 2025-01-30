2f7312862ea432a9bfc43df77f648f1e1b5e2412
Ehan Chowdhury <nibizsoft@gmail.com>
2025-01-30 19:03:02 +0600

    Updated changes

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Ehan Chowdhury",
     author_email="nibizsoft@gmail.com",
-    url="https://github.com/EhanChowdhury/pusher-aio",  # Replace with your GitHub or project URL
+    url="https://github.com/EhanChowdhury/pusheraio",  # Replace with your GitHub or project URL
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
